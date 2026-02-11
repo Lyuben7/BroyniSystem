badc68983e500ce4445693116296fba7e6eab39b
LA <lyuben.andreev@pgmtplovdiv.info>
2026-02-11 10:06:17 +0200

    Add project files.

---
+++ BroyniSystem/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace BroyniSystem
+{
+    internal class Program
+    {
+        private static void Main(String[] args)
+        {
+            Console.Write("Въведете десетично число: ");
+            int n = int.Parse(Console.ReadLine());
+            string binary = "";
+            while (n > 0)
+            {
+                binary = (n % 2) + binary;
+                n /= 2;
+            }
+            Console.WriteLine($"Двоично: {binary}");
+        }
+    }
+}
